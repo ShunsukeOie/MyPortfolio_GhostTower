54dddad1ab3fd8638ed6e9ffbb0c8b19fa1bb3ed
Aso <>
2024-10-05 21:25:17 +0900

    aso プレイヤーの向きの動き

--- Assets/Scripts/Player/Player_Move.cs
+++ Assets/Scripts/Player/Player_Move.cs
@@ -8,6 +8,9 @@ public class Player_Move : MonoBehaviour
     [SerializeField]
     [Header("移動速度")] public float m_MoveSpeed = 0.15f;
 
+    //プレイヤーの回転スピード
+    [SerializeField]
+    [Header("回転スピード")] public float m_RotationSpeed = 0.15f;
    
 
     // Start is called before the first frame update
@@ -20,13 +23,19 @@ public class Player_Move : MonoBehaviour
     void Update()
     {
 
-        //水平入力軸の値を取得する
+        //左スティックの水平入力軸の値を取得する
         float horizontakInput = Input.GetAxis("Horizontal");
 
-        //垂直入力軸の値を取得する
+        //左スティックの垂直入力軸の値を取得する
         float verticalInput = Input.GetAxis("Vertical");
 
-        //入力された定義された座標に移動
-        transform.Translate(new Vector3(horizontakInput, 0, verticalInput) * m_MoveSpeed * Time.deltaTime);
+        //左スティックの入力から定義された座標に移動
+        transform.localPosition += new Vector3(horizontakInput, 0, verticalInput) * m_MoveSpeed * Time.deltaTime;
+
+        //右スティックの水平入力軸の値を取得する
+        float horizontak2Input = Input.GetAxis("Horizontal2");
+
+        //右スティックでプレイヤーを回転
+        transform.Rotate(new Vector3(0, m_RotationSpeed * horizontak2Input,0));
     }
 }
