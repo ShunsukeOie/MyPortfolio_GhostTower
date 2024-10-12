6aa44304c40e4aceee7be7af2f9b59169705377b
ShunsukeOie <>
2024-10-13 00:21:19 +0900

    小家　ランタン作成

--- Assets/Scripts/Player/Player_lanthanum.cs
+++ Assets/Scripts/Player/Player_lanthanum.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 
 public class Player_lanthanum : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject ItemObj;
+    // 目の前にあるランタンを格納する為の変数
+    private GameObject LanthanumObj = null;
 
     RaycastHit hit;
     [SerializeField, Header("Rayのサイズ")]
     private Vector3 BoxSize;
 
-    [SerializeField,  Header("どの位置からRayを飛ばすか")]
-    private Vector3 AddPos;
-
     [SerializeField, Header("どの距離まで判定を取るか")]
-    private float Distance = 2f;
+    private float Distance;
 
 
     [Header("どのレイヤーの判定を取るか")]
@@ -25,50 +22,59 @@ public class Player_lanthanum : MonoBehaviour
     // アイテムが目の前にあるか
     private bool IsItem = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         //目の前にアイテムがあるかをチェック
         ItemCheck();
 
-        //　Fキーで取得
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            
-        }
+        // 光を灯す
+        LightUp();
     }
 
     void ItemCheck()
     {
-        //目の前に箱型のRayを飛ばして　アイテムがあるかを探す
-        if (Physics.BoxCast(gameObject.transform.position + AddPos, BoxSize, transform.forward,
+        // 目の前に箱型のレイを飛ばしてランタンがあるか判定する
+        if(Physics.BoxCast(transform.position, BoxSize, transform.forward,
             out hit, gameObject.transform.rotation, Distance, layerMask))
         {
-            //　目の前のアイテムの情報を格納
-            ItemObj = hit.collider.gameObject;
-            if (!IsItem)
+            // レイにヒットしたオブジェクトのタグがLanthanumだったら処理する
+            if (hit.collider.gameObject.tag == "Lanthanum")
             {
-                //UIを表示
-                UIImage.SetActive(true);
-                IsItem = true;
+                //　目の前のランタンの情報を格納
+                LanthanumObj = hit.collider.gameObject;
+
+                if (!IsItem)
+                {
+                    //UIを表示
+                    UIImage.SetActive(true);
+                    IsItem = true;
+                }
             }
         }
         else
         {
             //目の前に無かったら
             //アイテム情報を削除
-            ItemObj = null;
+            LanthanumObj = null;
             if (IsItem)
             {
                 //UIを非表示に
                 UIImage.SetActive(false);
                 IsItem = false;
+            }
+        }
+    }
+
+    void LightUp()
+    {
+        //　Fキーで取得
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if(LanthanumObj != null)
+            {
+                LanthanumObj.transform.Find("candle_").gameObject.GetComponent<Light>().enabled = true;
+                LanthanumObj.gameObject.tag = "none";
             }
         }
     }
