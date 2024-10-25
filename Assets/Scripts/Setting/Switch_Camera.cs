2f156dced31e5769e4afaa119934d2df16459305
Aso <>
2024-10-26 01:47:22 +0900

    aso カメラ切り替え時に移動処理を変更・ほかのシーンにも設定

--- Assets/Scripts/Setting/Switch_Camera.cs
+++ Assets/Scripts/Setting/Switch_Camera.cs
@@ -9,11 +9,20 @@ public class Switch_Camera : MonoBehaviour
     public Camera main_camera;
     public Camera sub_camera;
 
+    //プレイヤーオブジェクトを取得
+    [SerializeField] GameObject player_move;
+
     // Start is called before the first frame update
     void Start()
     {
         //一人称カメラを最初はオフにする
         sub_camera.enabled = false;
+
+        player_move = GameObject.Find("chara");
+
+        //初期操作の設定
+        player_move.GetComponent<Player_Move>().enabled = true;
+        player_move.GetComponent<Player_camera>().enabled = false;
     }
 
     // Update is called once per frame
@@ -22,17 +31,25 @@ public class Switch_Camera : MonoBehaviour
         //Cボタンを押したときの処理
         if (Input.GetKeyDown(KeyCode.C))
         {
+            //一人称モードにする
             if(!sub_camera.enabled)
             {
                 sub_camera.enabled = true;
 
                 main_camera.enabled = false;
+
+                player_move.GetComponent<Player_Move>().enabled = false;
+                player_move.GetComponent<Player_camera>().enabled = true;
             }
+            //見下ろし型にする
             else
             {
                 sub_camera.enabled = false;
 
                 main_camera.enabled = true;
+
+                player_move.GetComponent<Player_Move>().enabled = true;
+                player_move.GetComponent<Player_camera>().enabled = false;
             }
         }
     }
