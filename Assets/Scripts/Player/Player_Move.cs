4d5f46d6fc022da2d5a0acd77c7dddf37f3e2592
Aso <>
2024-10-29 23:44:33 +0900

    aso 机！

--- Assets/Scripts/Player/Player_Move.cs
+++ Assets/Scripts/Player/Player_Move.cs
@@ -12,13 +12,15 @@ public class Player_Move : MonoBehaviour
     [SerializeField]
     [Header("回転スピード")] public float RotationSpeed = 0.15f;
 
-
+    //Animatorの格納用
+    //Animator m_animator;
    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //アタッチされているコンポーネントを取得
+        //m_animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -35,14 +37,42 @@ public class Player_Move : MonoBehaviour
     //プレイヤーの移動用メソッド
     void PlayerMove()
     {
-        //左スティックの水平入力軸の値を取得する
+        //左スティックの水平入力軸の値を取得する(右：プラス、左：マイナス)
         float horizontakInput = Input.GetAxis("Horizontal");
 
-        //左スティックの垂直入力軸の値を取得する
+        //左スティックの垂直入力軸の値を取得する（上：プラス、下：マイナス）
         float verticalInput = Input.GetAxis("Vertical");
 
         //左スティックの入力から定義された座標に移動
         transform.localPosition += new Vector3(horizontakInput, 0, verticalInput) * MoveSpeed * Time.deltaTime;
+
+
+        //アニメーションの再生処理
+        //上
+        if(verticalInput > 0 && horizontakInput < 0.5 && horizontakInput < -0.5)
+        {
+            //m_animator.SetInteger("Action", 1);
+        }
+        //下
+        else if(verticalInput < 0 && horizontakInput < 0.5 && horizontakInput < -0.5)
+        {
+            //m_animator.SetInteger("Action", 2);
+        }
+        //右
+        else if(horizontakInput > 0 && verticalInput < 0.5 && verticalInput < -0.5)
+        {
+            //m_animator.SetInteger("Action", 3);
+        }
+        //左
+        else if (horizontakInput < 0 && verticalInput < 0.5 && verticalInput < -0.5)
+        {
+            //m_animator.SetInteger("Action", 4);
+        }
+        //待機状態
+        else
+        {
+           // m_animator.SetInteger("Action", 0);
+        }
     }
 
     //プレイヤーの回転用メソッド
